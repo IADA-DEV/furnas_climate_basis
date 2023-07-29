6e5b7d4c6579fbdd30e896ac8ac5d72e59b722f6
NortonRicardo <nortonricardo@live.com>
2023-07-28 22:43:16 -0300

    teste deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@
 # )
 
 
-User.create(name: 'Development', email: 'teste@dev.com', password: 231193!, password_confirmation: 231193!)
+User.create(name: 'Development', email: 'teste@dev.com', password: '231193!', password_confirmation: '231193!')
 
 # obj_s = [
 #     {
