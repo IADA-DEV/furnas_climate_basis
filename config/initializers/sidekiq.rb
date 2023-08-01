8d27d0520f9f3484a1b9650a102c6798e70806c7
NortonRicardo <nortonricardo@live.com>
2023-07-31 21:50:35 -0300

    Add restrição para executar somente em prod

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,14 +9,26 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
 end
 
-('00'..'23').to_a.each do |hour|
-  Sidekiq::Cron::Job.create(
-    name: "INMET - IMPORT DIARIO - #{hour}", # nome do trabalho
-    cron: "0 #{hour} * * *", # tempo em cron notation, neste caso, a cada hora
-    class: 'DataImportTodayInmetWorker', # nome da classe worker
-    args: "'#{hour}00'" # Argumentos para a função perform
-  )
+# ('00'..'23').to_a.each do |hour|
+#   Sidekiq::Cron::Job.create(
+#     name: "INMET - IMPORT DIARIO - #{hour}", # nome do trabalho
+#     cron: "0 #{hour} * * *", # tempo em cron notation, neste caso, a cada hora
+#     class: 'DataImportTodayInmetWorker', # nome da classe worker
+#     args: "'#{hour}00'" # Argumentos para a função perform
+#   )
+# end
+
+if Rails.env.production?
+  ('00'..'23').to_a.each do |hour|
+    Sidekiq::Cron::Job.create(
+      name: "INMET - IMPORT DIARIO - #{hour}", # nome do trabalho
+      cron: "0 #{hour} * * *", # tempo em cron notation, neste caso, a cada hora
+      class: 'DataImportTodayInmetWorker', # nome da classe worker
+      args: "'#{hour}00'" # Argumentos para a função perform
+    )
+  end
 end
+
 
 # Sidekiq::Cron::Job.create(
 #   name: "Nome do Job - 21:10", # nome do trabalho
