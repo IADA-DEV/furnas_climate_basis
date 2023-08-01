1d90c6e8d1e71b2ea084e59903a7516178a8d888
NortonRicardo <nortonricardo@live.com>
2023-08-01 19:12:47 -0300

    correção de jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,20 +9,11 @@ Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
 end
 
-# ('00'..'23').to_a.each do |hour|
-#   Sidekiq::Cron::Job.create(
-#     name: "INMET - IMPORT DIARIO - #{hour}", # nome do trabalho
-#     cron: "0 #{hour} * * *", # tempo em cron notation, neste caso, a cada hora
-#     class: 'DataImportTodayInmetWorker', # nome da classe worker
-#     args: "'#{hour}00'" # Argumentos para a função perform
-#   )
-# end
-
 if Rails.env.production?
   ('00'..'23').to_a.each do |hour|
     Sidekiq::Cron::Job.create(
       name: "INMET - IMPORT DIARIO - #{hour}:00", # nome do trabalho
-      cron: "20 #{hour} * * *", # tempo em cron notation, neste caso, a cada hora
+      cron: "*/10 #{hour} * * *", # tempo na notação cron, neste caso, a cada 10 minutos dentro da hora
       class: 'DataImportTodayInmetWorker', # nome da classe worker
       args: ["#{hour}00"] # Argumentos para a função perform
     )
@@ -30,9 +21,4 @@ if Rails.env.production?
 end
 
 
-# Sidekiq::Cron::Job.create(
-#   name: "Nome do Job - 21:10", # nome do trabalho
-#   cron: "10 21 * * *", # tempo em cron notation, neste caso, todos os dias às 21:10
-#   class: 'DataImportTodayInmetWorker', # nome da classe worker
-#   args: "'2100'" # Argumentos para a função perform
-# )
+
