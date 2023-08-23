4441442afd6d48b676a0767e0dcdce55c5ada8b8
NortonRicardo <nortonricardo@live.com>
2023-08-23 18:03:28 -0300

    Add graficos

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,48 +1,95 @@
 class DashboardController < ApplicationController
-
+  before_action :cidade, only: [:index]
+  before_action :range, only: [:index]
   def index
-    @station = InmetWeatherStation.all
-    # @size_station = @station.count
-    # @operante = @station.where(situacao: "Operante").size
-    # @pane = @station.where(situacao: "Pane").size
-    @cidade = @station.collect{ |t| t.cidade }.uniq
 
-    # @data = InmetWeatherDatum.all.size
+    respond_to do |format|
+      if turbo_frame_request?
+        if params[:data_inicio].present?
+          data_weather
+          dados_diarios
+          partial = 'table_diaria'
+        else
+          partial = 'table_range'
+          data_weather_range
+        end
+        format.html { render partial: partial}
+      else
+        data_weather
+        dados_diarios
+        data_weather_range
+        format.html
+      end
+    end
 
-    @inmet_weather_data = InmetWeatherDatum
-                            .filter(by_cdg_station: 'A002',
-                                    by_dta_medicao: Date.current.to_s)
-                            .order(:inmet_weather_station_id, :dta_medicao, :hr_medicao)
-
-    @tem_ins = @inmet_weather_data.collect { |t| t.tem_ins.to_i }.compact
-    @umd_ins = @inmet_weather_data.collect { |t| t.umd_ins.to_i }.compact
-    @rad_glo = @inmet_weather_data.collect { |t| t.rad_glo }.compact
-    @chuva = @inmet_weather_data.collect { |t| t.chuva }.compact
-    @pre_ins = @inmet_weather_data.collect { |t| t.pre_ins }.compact
-    @ven_vel = @inmet_weather_data.collect { |t| t.ven_vel }.compact
-
-
-    @time = @inmet_weather_data.collect { |t| t.hr_medicao.to_time_br }
-
-
-    # @tem_ins_normalized = min_max_scale(@tem_ins)
-    # @umd_ins_normalized = min_max_scale(@umd_ins)
-    @rad_glo_normalized = min_max_scale(@rad_glo)
-    # @chuva_normalized = min_max_scale(@chuva)
-    @pre_ins_normalized = min_max_scale(@pre_ins)
-    # @ven_vel_normalized = min_max_scale(@ven_vel)
   end
 
   def grafico
   end
 
-  # Min-max scaling function
-  def min_max_scale(arr)
-    min_val = arr.min
-    max_val = arr.max
-    scaled_arr = arr.map { |val| (((val - min_val) / (max_val - min_val)) * 100).round(2) }
-    return scaled_arr
+  private
+
+  def dados_diarios
+    @tem_ins = @inmet_weather_data.collect { |t| t.tem_ins.to_f.round(2) }.compact
+    @umd_ins = @inmet_weather_data.collect { |t| t.umd_ins.to_f.round(2) }.compact
+    @rad_glo = @inmet_weather_data.collect { |t| t.rad_glo.to_f.round(2) }.compact
+    @chuva = @inmet_weather_data.collect { |t| t.chuva.to_f.round(2) }.compact
+    @pre_ins = @inmet_weather_data.collect { |t| t.pre_ins.to_f.round(2) }.compact
+    @ven_vel = @inmet_weather_data.collect { |t| t.ven_vel.to_f.round(2) }.compact
+    @time = @inmet_weather_data.collect { |t| t.hr_medicao.to_time_br }
   end
 
+  def cidade
+    @cidade = InmetWeatherStation.all.collect{ |t| t.cidade }.uniq
+  end
+
+  def data_weather
+    @inmet_weather_data = InmetWeatherDatum
+                            .filter(by_cdg_station: 'A002',
+                                    by_dta_medicao: params[:data_inicio] || Date.current.to_s)
+                            .order(:inmet_weather_station_id, :dta_medicao, :hr_medicao)
+  end
+
+  def data_weather_range
+    @inmet_weather_data_range = InmetWeatherDatum
+                            .filter(by_cdg_station: 'A002')
+                            .where(dta_medicao: params[:range_data_inicio]..params[:range_data_fim])
+                            .group(:dta_medicao)
+                            .select(
+                              :dta_medicao,
+                              "AVG(pre_ins) AS avg_pre_ins",
+                              "AVG(tem_sem) AS avg_tem_sem",
+                              "AVG(pre_max) AS avg_pre_max",
+                              "AVG(rad_glo) AS avg_rad_glo",
+                              "AVG(pto_ins) AS avg_pto_ins",
+                              "AVG(tem_min) AS avg_tem_min",
+                              "AVG(umd_min) AS avg_umd_min",
+                              "AVG(pto_max) AS avg_pto_max",
+                              "AVG(ven_dir) AS avg_ven_dir",
+                              "AVG(chuva) AS avg_chuva",
+                              "AVG(pre_min) AS avg_pre_min",
+                              "AVG(umd_max) AS avg_umd_max",
+                              "AVG(ven_vel) AS avg_ven_vel",
+                              "AVG(pto_min) AS avg_pto_min",
+                              "AVG(tem_max) AS avg_tem_max",
+                              "AVG(ven_raj) AS avg_ven_raj",
+                              "AVG(tem_ins) AS avg_tem_ins",
+                              "AVG(umd_ins) AS avg_umd_ins"
+                            )
+                            .order(:dta_medicao)
+
+    @tem_ins_range = @inmet_weather_data_range.collect { |t| t.avg_tem_ins.to_f.round(2) }.compact
+    @umd_ins_range = @inmet_weather_data_range.collect { |t| t.avg_umd_ins.to_f.round(2) }.compact
+    @rad_glo_range = @inmet_weather_data_range.collect { |t| t.avg_rad_glo.to_f.round(2) }.compact
+    @chuva_range = @inmet_weather_data_range.collect { |t| t.avg_chuva.to_f.round(2) }.compact
+    @pre_ins_range = @inmet_weather_data_range.collect { |t| t.avg_pre_ins.to_f.round(2) }.compact
+    @ven_vel_range = @inmet_weather_data_range.collect { |t| t.avg_ven_vel.to_f.round(2) }.compact
+    @time_range = @inmet_weather_data_range.collect { |t| t.dta_medicao.to_date_b }
+  end
+
+  def range
+    params[:range_data_fim] = Date.current.to_s if !params[:range_data_fim]
+    params[:range_data_inicio] = (params[:range_data_fim].to_date - 30.day).to_s if !params[:range_data_inicio]
+  end
 
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin "application", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.7/lib/index.js"
 pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.7.12/dist/sweetalert2.all.js"
+pin "@hotwired/turbo-rails", to: "turbo.js"
